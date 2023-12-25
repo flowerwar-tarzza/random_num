9a2c269e32001de8c2a66e2ed56300abfa80c827
flowerwar-tarzza <anthonypeaceinspace@gmail.com>
2023-12-25 17:29:38 +0900

    random/target delete

--- random/target/debug/deps/random-ff3c4cefd9e9650b.d
+++
@@ -1,5 +0,0 @@
-/home/rambo3/project/rust/random_git/random/target/debug/deps/random-ff3c4cefd9e9650b: src/main.rs
-
-/home/rambo3/project/rust/random_git/random/target/debug/deps/random-ff3c4cefd9e9650b.d: src/main.rs
-
-src/main.rs:
--- random/target/debug/random.d
+++
@@ -1,1 +0,0 @@
-/home/rambo3/project/rust/random_git/random/target/debug/random: /home/rambo3/project/rust/random_git/random/src/main.rs
