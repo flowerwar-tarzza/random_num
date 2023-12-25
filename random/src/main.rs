7031ce0f7390e8647b45e1fecb239185be31e757
flowerwar-tarzza <anthonypeaceinspace@gmail.com>
2023-12-25 18:43:06 +0900

    파일 레코드 분해중

--- random/src/main.rs
+++ random/src/main.rs
@@ -1,10 +1,39 @@
 use std::fs;
 
+struct Memo{
+    word:String,
+    pornounce:String,
+    meanings:Vec<String>,
+    ex_sentence:Vec<String>,
+}
+
+impl Memo {
+    fn build(word:String,pornounce:String,meanings:Vec<String>,ex_sentence:Vec<String>) -> Memo {
+        Memo{
+            word,
+            pornounce,
+            meanings,
+            ex_sentence,
+            }
+    }
+}
 fn main() {
     let read_file = match fs::read_to_string("look_again_test.csv") {
         Ok(result) => result,
         Err(e) => panic!("{e}"),
     };
-    println!("result {read_file}");
 
+    let v:Vec<&str> = read_file.split('\n').collect();
+
+    for (i,line) in v.iter().enumerate() {
+        if i < 1 {
+            continue;
+        }
+        let row:Vec<_> = line.split('\t').collect();
+        
+        for col in row {
+            println!("{}",col);
+        }
+        //println!("{}=> {}]",i,line);
+    }
 }
