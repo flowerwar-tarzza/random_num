12422da01c44dad264f8c8df664882d38e73fec2
flowerwar-tarzza <anthonypeaceinspace@gmail.com>
2023-12-25 20:59:56 +0900

    워드 스트럭처-필드 구분입력

--- random/src/main.rs
+++ random/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-
+#[derive(Debug)]
 struct Memo{
     word:String,
     pornounce:String,
@@ -8,14 +8,16 @@ struct Memo{
 }
 
 impl Memo {
-    fn build(word:String,pornounce:String,meanings:Vec<String>,ex_sentence:Vec<String>) -> Memo {
+    //fn build(word:String,pornounce:String,meanings:Vec<String>,ex_sentence:Vec<String>) -> Memo {
+    fn build() -> Memo {
         Memo{
-            word,
-            pornounce,
-            meanings,
-            ex_sentence,
+            word : String::new(),
+            pornounce: String::new(),
+            meanings : Vec::new(),
+            ex_sentence: Vec::new(),
             }
     }
+
 }
 fn main() {
     let read_file = match fs::read_to_string("look_again_test.csv") {
@@ -23,17 +25,31 @@ fn main() {
         Err(e) => panic!("{e}"),
     };
 
-    let v:Vec<&str> = read_file.split('\n').collect();
+    let lines:Vec<&str> = read_file.split('\n').collect();
 
-    for (i,line) in v.iter().enumerate() {
+    for (i,line) in lines.iter().enumerate() {
         if i < 1 {
             continue;
         }
-        let row:Vec<_> = line.split('\t').collect();
-        
-        for col in row {
-            println!("{}",col);
+        let mut one_memo = Memo::build();
+        let mut cols:Vec<_> = line.split('\t').collect();
+        one_memo.word = String::from(cols.remove(0));
+        one_memo.pornounce = String::from(cols.remove(0));
+        println!("row length: {}",cols.len());
+        let mut flag_ex_sentence: bool = false;
+        for col in cols {
+            //println!("col size:{}//{}",col,col.len());
+            if col.len() > 0 && !flag_ex_sentence { // meaning add
+                one_memo.meanings.push(String::from(col));
+            } else {
+                flag_ex_sentence = true;
+            }
+            if col.len() > 0 && flag_ex_sentence { // meaning add
+                one_memo.ex_sentence.push(String::from(col));
+            }
         }
-        //println!("{}=> {}]",i,line);
+        println!("{:#?}]",one_memo);
+        if i > 3 {break;}  // 4 개까지 만  출력위해
     }
+
 }
