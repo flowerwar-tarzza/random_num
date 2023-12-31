0f5b8bee4245a83e3f33416c571ad01807c2c183
flowerwar-tarzza <anthonypeaceinspace@gmail.com>
2023-12-31 19:03:18 +0900

    command lin wme : display

--- random/src/main.rs
+++ random/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     let mut file_name:String = String::new();
     let mut start:usize = 1;
     let mut amount:usize = 1;
+    let mut method:MemoShowMethod = MemoShowMethod::WORD;
 
     args.next();
     println!("args len:{}",args.len());
@@ -17,7 +18,20 @@ fn main() {
         3 => { file_name = args.next().unwrap();
             start = args.next().unwrap().parse().unwrap();
             amount = args.next().unwrap().parse().unwrap(); },
-        _ => {println!(" ex)file_name start amount!"); return;},
+        4 => {
+            file_name = args.next().unwrap();
+            start = args.next().unwrap().parse().unwrap();
+            amount = args.next().unwrap().parse().unwrap();
+            let temp_method:&str = &String::from(args.next().unwrap().trim());
+            //let temp_method = args.next().unwrap().trim(); // <---- 다시보기 의문
+            match temp_method {
+                "w" => { method = MemoShowMethod::WORD; },
+                "wm" => { method = MemoShowMethod::WORD_MEAN; },
+                "wme" => { method = MemoShowMethod::WORD_MEAN_EXAMPLE; },
+                &_ => { println!("incorrect show method !"); },
+            }
+        },
+        _ => {println!(" ex)file_name start amount show_method:w,wm,wme!"); return;},
     }
 
     for argument in args {
@@ -28,6 +42,6 @@ fn main() {
     let memo_manager = MemoManager::build(book);
 
     println!("시작부터 : 볼 갯수");
-    memo_manager.display_memo(MemoShowRange::Select(start,amount),MemoShowMethod::WORD_MEAN_EXAMPLE);
+    memo_manager.display_memo(MemoShowRange::Select(start,amount),method);
     //memo_manager.display_memo(MemoShowRange::All);
 }
